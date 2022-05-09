8a2aaee1bc04b53b93e3b8ec7cd52ecafa2e6a0b
Kamil Frączek <kamx9@o2.pl>
2022-05-09 09:12:40 +0200

    poprawa rotacji

--- src/main/java/point/PointConverter.java
+++ src/main/java/point/PointConverter.java
@@ -6,9 +6,9 @@ import java.awt.*;
 
 public class PointConverter {
 
-    public static Point convertPerspectivePoint(MyPoint point3D, double scale) {
+    private static final double d = 800;
 
-        double d = 200;
+    public static Point convertPerspectivePoint(MyPoint point3D, double scale) {
 
         double x2d = point3D.x / ( 1 + (point3D.z / d));
         double y2d = point3D.y / ( 1 + (point3D.z / d));
@@ -23,28 +23,31 @@ public class PointConverter {
 
     public static MyPoint rotateAxisX(MyPoint p, boolean direct, double degrees){
 
-        double radius = Math.sqrt(p.y*p.y + p.z*p.z);
-        double t = Math.atan2(p.z, p.y);
+        double pz = p.z + d;
+        double radius = Math.sqrt(p.y*p.y + pz*pz);
+        double t = Math.atan2(pz, p.y);
 
         t += 2 * Math.PI / 360 * degrees * (direct?-1:1);
 
         double y = radius * Math.cos(t);
         double z = radius * Math.sin(t);
 
-        return new MyPoint(p.x, y, z);
+        return new MyPoint(p.x, y, z - d);
     }
 
     public static MyPoint rotateAxisY(MyPoint p, boolean direct, double degrees){
 
-        double radius = Math.sqrt(p.x*p.x + p.z*p.z);
-        double t = Math.atan2(p.x, p.z);
+        double pz = p.z + d;
+
+        double radius = Math.sqrt(p.x*p.x + pz*pz);
+        double t = Math.atan2(p.x, pz);
 
         t += 2 * Math.PI / 360 * degrees * (direct?-1:1);
 
         double x = radius * Math.sin(t);
         double z = radius * Math.cos(t);
 
-        return new MyPoint(x, p.y, z);
+        return new MyPoint(x, p.y, z - d);
     }
 
     public static MyPoint rotateAxisZ(MyPoint p, boolean direct, double degrees){
